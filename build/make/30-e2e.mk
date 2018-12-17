49a17e9a4a4b3b05e0be6a6c9bb3169d8f0d141c
dmaiocchi <dmaiocchi@suse.com>
2018-12-17 15:26:39 +0100

    Add ginkgo pkg dep

--- build/make/30-e2e.mk
+++ build/make/30-e2e.mk
@@ -4,5 +4,6 @@
 
 SEEDER := $(shell cd $(TF_LIBVIRT_FULL_DIR) && terraform output -json seeder 2>/dev/null | python -c 'import sys, json; print json.load(sys.stdin)["value"]' 2>/dev/null || echo "unknown")
 tf-e2e-tests:
+	$(GO_NOMOD) get -u github.com/onsi/ginkgo/ginkgo
 	cd tests && SEEDER=$(SEEDER) ./run_suites.sh
 
